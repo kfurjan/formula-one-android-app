5fa9cfa8b029a858f363612a1b6e487db0753be6
Kevin Furjan <kfurjan@gmail.com>
2021-01-18 14:04:33 +0100

    Mark data as imported once saving data to SQLlite is finished

--- app/src/main/java/hr/algebra/formula1/Formula1DataService.kt
+++ app/src/main/java/hr/algebra/formula1/Formula1DataService.kt
@@ -4,11 +4,13 @@ import android.content.Context
 import android.content.Intent
 import androidx.core.app.JobIntentService
 import hr.algebra.formula1.api.Formula1DataFetcher
+import hr.algebra.formula1.extensions.setBooleanPreference
 
 private const val JOB_ID = 1
 
 class Formula1DataService : JobIntentService() {
     override fun onHandleWork(intent: Intent) {
+        applicationContext.setBooleanPreference(DATA_IMPORTED, true)
         Formula1DataFetcher(this).fetchData()
     }
 
